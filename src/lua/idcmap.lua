facac72a09cafab6e644a0d325c8bd6879853a55
YongMan <yming0221@gmail.com>
2015-08-20 12:10:09 +0800

    Fix treat nj=>nj02, hz=>hz01

--- src/lua/idcmap.lua
+++ src/lua/idcmap.lua
@@ -8,19 +8,19 @@ primary = {
 }
 
 primary_preferred = {
-   tc = {"$master","tc","jx",{"nj02","nj03","hz01"}},
-   jx = {"$master","jx","tc",{"nj02","nj03","hz01"}},
-   nj02 = {"$master","nj02","nj03","hz01",{"tc","jx"}},
-   nj03 = {"$master","nj03","nj02","hz01",{"tc","jx"}},
-   hz01 = {"$master","hz01",{"nj02","nj03"},{"tc","jx"}}
+   tc = {"$master","tc","jx",{"nj","nj03","hz"}},
+   jx = {"$master","jx","tc",{"nj","nj03","hz"}},
+   nj = {"$master","nj","nj03","hz",{"tc","jx"}},
+   nj03 = {"$master","nj03","nj","hz",{"tc","jx"}},
+   hz = {"$master","hz",{"nj","nj03"},{"tc","jx"}}
 }
 
 nearest = {
-   tc = {"tc","jx",{"nj02","nj03","hz01"}},
-   jx = {"jx","tc",{"nj02","nj03","hz01"}},
-   nj02 = {"nj02","nj03","hz01",{"tc","jx"}},
-   nj03 = {"nj03","nj02","hz01",{"tc","jx"}},
-   hz01 = {"hz01",{"nj02","nj03"},{"tc","jx"}}
+   tc = {"tc","jx",{"nj","nj03","hz"}},
+   jx = {"jx","tc",{"nj","nj03","hz"}},
+   nj = {"nj","nj03","hz",{"tc","jx"}},
+   nj03 = {"nj03","nj","hz",{"tc","jx"}},
+   hz = {"hz",{"nj","nj03"},{"tc","jx"}}
 }
 
 return nearest
