e63176f815ad936a06826bd304b56667a006846a
YongMan <yming0221@gmail.com>
2015-09-08 13:25:17 +0800

    Add sh and gzhxy idc

--- src/lua/idcmap.lua
+++ src/lua/idcmap.lua
@@ -16,11 +16,13 @@ primary_preferred = {
 }
 
 nearest = {
-   tc = {"tc","jx",{"nj","nj03","hz"}},
-   jx = {"jx","tc",{"nj","nj03","hz"}},
-   nj = {"nj","nj03","hz",{"tc","jx"}},
-   nj03 = {"nj03","nj","hz",{"tc","jx"}},
-   hz = {"hz",{"nj","nj03"},{"tc","jx"}}
+   tc = {"tc","jx",{"nj","nj03","hz","sh"},"gzhxy"},
+   jx = {"jx","tc",{"nj","nj03","hz","sh"},"gzhxy"},
+   nj = {"nj",{"nj03","hz","sh"},{"tc","jx"},"gzhxy"},
+   nj03 = {"nj03",{"nj","hz","sh"},{"tc","jx"},"gzhxy"},
+   hz = {"hz",{"nj","nj03","sh"},{"tc","jx"},"gzhxy"},
+   sh = {"sh",{"nj","nj03","hz"},"gzhxy",{"tc","jx"}},
+   gzhxy = {"gzhxy", {"nj","nj03","hz"},"sh",{"tc","jx"}}
 }
 
 return nearest
