49e2cb4ff09ffaba16d96a7d15ded1dfb9b58b1d
YongMan <yming0221@gmail.com>
2015-12-24 12:26:13 +0800

    Fix sort command can take multiple arguments

--- src/proto/nc_redis.c
+++ src/proto/nc_redis.c
@@ -75,7 +75,6 @@ redis_arg0(struct msg *r)
     case MSG_REQ_REDIS_EXISTS:
     case MSG_REQ_REDIS_PERSIST:
     case MSG_REQ_REDIS_PTTL:
-    case MSG_REQ_REDIS_SORT:
     case MSG_REQ_REDIS_TTL:
     case MSG_REQ_REDIS_TYPE:
     case MSG_REQ_REDIS_DUMP:
@@ -224,6 +223,8 @@ static bool
 redis_argn(struct msg *r)
 {
     switch (r->type) {
+    case MSG_REQ_REDIS_SORT:
+
     case MSG_REQ_REDIS_BITCOUNT:
 
     case MSG_REQ_REDIS_SET:
